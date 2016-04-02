d4f2dccd6717b94fda9369e37862129f48faf14c
Luke Winslow <lwinslow@usgs.gov>
2016-04-01 19:11:21 -0500

    setting up workflow for lake data, trends and metadata

---
+++ R/calculate_lake_slopes.R
@@ -0,0 +1,28 @@
+#calculate_lake_slopes
+
+calculate_lake_slopes = function(infile){
+  
+  lake_d = read.csv(infile)
+  
+  lake_d$yday = yday(lake_d$date)
+  lake_d$month = month(lake_d$date)
+  lake_d = na.omit(lake_d)
+  
+  #lmer(wtemp~year + (1|site_id) + (1|depth) + (1|yday), data = lake_d)
+  
+  lake_slopes = sens_seasonal_site(lake_d$year, lake_d$wtemp, lake_d$month, paste0(lake_d$site_id, ":", lake_d$depth))
+  
+  rm(lake_d)
+  
+  ds_split = strsplit(as.character(lake_slopes$sites_i), ":")
+  lake_slopes$site_id = sapply(ds_split, function(x){x[1]})
+  lake_slopes$depth = sapply(ds_split, function(x){x[2]})
+  lake_slopes$depth = as.numeric(lake_slopes$depth)
+  
+  lake_slopes$sites_i = NULL
+  
+  lake_slopes = subset(lake_slopes, !is.na(slopes) & !is.infinite(slopes))
+  
+  save(lake_slopes, file='data/temperature_lakes/slope_population.Rdata')
+  
+}
