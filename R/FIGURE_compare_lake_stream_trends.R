4acf571214b51db7a81aeacf7664bafb1312972e
Luke Winslow <lwinslow@usgs.gov>
2016-05-05 13:16:20 -0500

    change to 25 years of stream temp, add lake/stream map and fix remake

--- R/FIGURE_compare_lake_stream_trends.R
+++ R/FIGURE_compare_lake_stream_trends.R
@@ -1,5 +1,7 @@
 #FIGURE_compare_lake_stream_trends
 
+library(maps)
+
 lakes = read.csv('data/trends/lakes_gltc_jas.csv', as.is=TRUE)
 streams = read.csv('data/trends/streams_gltc_jas.csv', as.is=TRUE)
 
@@ -12,7 +14,7 @@ png('output/lake.vs.stream.scatter.png', res=300, width=2100, height=1600)
 #par(mfrow=c(2,1), mar=c(0,4,1,1), oma=c(4,0,0,0))
 
 #scatter plot
-plot(lakes$sen_airt, lakes$trend, col=lcol, pch=16, ylim=c(-0.25,0.25), xlim=c(-0.1,0.15), xlab='', ylab='')
+plot(lakes$sen_airt, lakes$trend, col=lcol, pch=16, ylim=c(-0.25,0.25), xlim=c(-0.15,0.15), xlab='', ylab='')
 points(streams$sen_airt, streams$sen_wtr, col=scol, pch=16)
 
 legend('bottomleft', pch=16, col=c(lcol, scol), legend = c('Lakes', 'Streams'))
@@ -30,6 +32,8 @@ abline(lm(trend~sen_airt, data=lakes), col=lcol)
 abline(lm(sen_wtr~sen_airt, data=streams), col=scol)
 
 dev.off()
+
+##Lake and stream trend density plots
 
 png('output/lake.vs.stream.density.png', res=300, width=2100, height=1600)
 
@@ -50,3 +54,17 @@ mtext(expression(Water~temp~trend("°C"~yr^-1)), side=1, line=3)
 mtext('Relative frequency', side=2, line=2.5)
 
 dev.off()
+
+
+## Lake and stream map 
+png('output/lake.vs.stream.map.png', res=300, width=2100, height=1600)
+
+map('usa')
+
+points(lakes$longitude, lakes$latitude, col=lcol, pch=16)
+points(streams$dec_long_va, streams$dec_lat_va, pch=16, col=scol)
+
+legend('bottomleft', pch=16, col=c(lcol, scol), legend = c('Lakes', 'Streams'))
+
+dev.off()
+
